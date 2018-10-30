5a35f8096072841acf47b0ea32e74620b2e5dc8b
masakichi-yds <blogyds59weed@gmail.com>
2018-10-31 00:56:59 +0900

    spage_change

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  #Homeページのテスト
   test "should get home" do
+    #GETリクエストをhomeアクションに対して発行 (=送信) せよ。
     get static_pages_home_url
+    #そうすれば、リクエストに対するレスポンスは[成功]になるはず。
     assert_response :success
   end
 
@@ -11,4 +14,5 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  
 end
